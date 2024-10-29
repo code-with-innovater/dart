4b089603fc4817cdba3c70a23838d658b869a263
code-with-innovater <st168035@gmail.com>
2024-10-29 13:31:25 +0530

    basic

--- hello.dart
+++ hello.dart
@@ -1,3 +1,11 @@
 void main(){
-  print("hello world");
+  int firstvalue = 20;
+  int secondvalue = 4;
+  print(firstvalue);
+  print(secondvalue);
+  print(firstvalue+secondvalue);
+  print(firstvalue-secondvalue);
+  print(firstvalue*secondvalue);
+  print(firstvalue/secondvalue);
+  print(firstvalue%secondvalue);
 }
