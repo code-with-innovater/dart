f505a5cb0169ea083e1ab44b3a70c1f48aa7e5bc
code-with-innovater <st168035@gmail.com>
2024-10-30 13:32:01 +0530

    condition

--- condition.dart
+++ condition.dart
@@ -1,12 +1,19 @@
 void main(){
-  int income= 1400000;
-  double tax;
-  if(income<500000){
-    tax = 0;
-  }else if(income>=500000 && income <1000000){
-     tax = income * 0.2;
+  // int income= 1400000;
+  // double tax;
+  // if(income<500000){
+  //   tax = 0;
+  // }else if(income>=500000 && income <1000000){
+  //    tax = income * 0.2;
+  // }else{
+  //   tax = income*0.3;
+  // }
+  // print(tax);
+  int age = 18;
+  bool isallowed = false;
+  if(age!=18 && !isallowed){
+    print("haaahhaa");
   }else{
-    tax = income*0.3;
+    print("nahahsa");
   }
-  print(tax);
   }
