2c18ade22b72187b7a213e6633a8d798be67f76f
code-with-innovater <st168035@gmail.com>
2024-10-30 12:30:54 +0530

    variablr

---
+++ variable.dart
@@ -0,0 +1,11 @@
+void main(){
+
+    var name = 10;
+    print(name);
+    final somevalue2 = DateTime.now();
+    const somevalue3 = 1200;
+    print(somevalue2);
+    print(somevalue3);
+    String? somevalue;
+    print(somevalue);
+}
