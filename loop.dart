b3104f124eb79cb126125cb7a7f73ee54aef4be7
code-with-innovater <st168035@gmail.com>
2024-11-02 11:46:04 +0530

    understanding loop

---
+++ loop.dart
@@ -0,0 +1,33 @@
+// void main(){
+  // for (int i = 1; i<=10;i++){
+  //   String s1 = "hello world";
+  //   print(s1.substring(0,4));
+  //   // print("hello world");
+  //       print("hello world $i");
+// }
+// String value = "Hello";
+// int i = 0;
+// do {
+//   print(value[i]);
+//   i++;
+// }while(i!=1);
+// void myname(){
+//   print("sudhir tiwari");
+// }
+// }
+// void main(){
+//   var name = printname();
+//   print (name);
+
+// }
+// (int,String,bool,) printname(){
+//   return (10,"hello sudhir",false);
+// }
+void main(){
+  int age = 25;
+  print("my age after 1 year is ${age + 1}");
+  var name = "Sudhir"; // Dart infers that name is of type String
+var year = 2024; // Dart infers that age is of type int
+print(name);
+print (year);
+}
